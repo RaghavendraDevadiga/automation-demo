cdc25ad699e41f79c46a6b2b86e33ff66a19566a
Raghavendra <raghuadhu@gmail.com>
2018-09-01 05:00:40 +0530

    SimpleCalculator

---
+++ src/sample/cicd/demo/SimpleCalculator.java
@@ -0,0 +1,16 @@
+package sample.cicd.demo;
+
+public class SimpleCalculator {
+	public static void main(String[] args) {
+		System.out.println("Entering into the Simple Calculator");
+		int num1 = 4,num2=7;
+		SimpleCalculator calculator = new SimpleCalculator();
+		
+		System.out.println("Sum of num1 "+ num1 + " num2 "+ num2 + " is:"+ calculator.add(num1,num2));
+	}
+	
+	public int add(int num1,int num2){
+		int sum = num1 + num2;
+		return sum;
+	}
+}
